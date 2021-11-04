58645eff0cd087f2179d22abbf6129389e8d1ed0
ezzeldenWork <ezzelden.work@gmail.com>
2021-11-04 15:32:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import {useQuery, gql} from "@apollo/client";
+import {useState} from "react";
 
+const COUNTRY = gql`
+  query Country($code: ID!) {
+    country(code: $code) {
+      code 
+      name
+      emoji
+      nameWithEmoji @client
+    }
+  }
+`;
 function App() {
+  const [code, setCode] = useState("");
+
+  const {data, loading, error} = useQuery(COUNTRY, {
+    variables: {code: code},
+    skip: code.length !== 2,
+  });
+
+  const hendleCheng = (e) => {
+    setCode(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {error && <h1> {`You Broke it! ${error.message} `} </h1>}
+      {!data || loading ? (
+        <h1>Loding</h1>
+      ) : (
+        <h1>
+          {data.country.nameWithEmoji}
+        </h1>
+      )}
+      <input type="text" value={code} onChange={hendleCheng} />
     </div>
   );
 }
